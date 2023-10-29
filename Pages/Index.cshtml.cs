a9e3b7e0f6a02460fd43b0dffdcd0fe0b3ae3980
n-buettner <nick.buettner29@gmail.com>
2023-10-29 18:41:55 -0500

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NickB.WorkBoard.Pages;
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
